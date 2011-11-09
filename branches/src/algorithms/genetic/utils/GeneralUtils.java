bd47afb6079679acbf437e17cfca9eaa51593e45
pawel.batko@gmail.com <pawel.batko@gmail.com@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-09 14:33:12 +0000

    First implementation of genetic crossover operator

--- branches/src/algorithms/genetic/utils/GeneralUtils.java
+++ branches/src/algorithms/genetic/utils/GeneralUtils.java
@@ -4,17 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GeneralUtils {
-	
-	public static double nextDouble(double min, double max){
-		return Math.random()*(max-min)+min;
+
+	public static double nextRandomDouble(double min, double max) {
+		return Math.random() * (max - min) + min;
 	}
-	
-	
-	public static List<Integer> getIntegersFromOneTo(int lastInt){
+
+	public static int nextRandomIntModulo(int modulo) {
+		if (modulo == 0) {
+			return 0;
+		} else {
+			return (int) (nextRandomDouble(0, modulo - 1.0));
+		}
+	}
+
+	public static List<Integer> getIntegersFromOneTo(int lastInt) {
 		List<Integer> list = new ArrayList<>();
-		for (int i = 1; i < lastInt+1; i++) {
+		for (int i = 1; i < lastInt + 1; i++) {
 			list.add(i);
 		}
 		return list;
+	}
+	
+	public static boolean getBoolean(double probabilty){
+		if(probabilty>=1){
+			return true;
+		} else if(probabilty<=0){
+			return false;
+		} else {
+			if(Math.random()<probabilty){
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 }

123a21f50909d759d40ef81a021c889475c120c0
orzech.orzechowski <orzech.orzechowski@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-09 23:52:16 +0000

    drobne poprawki

---
+++ model/pizzeria/Order.java
@@ -0,0 +1,18 @@
+package model.pizzeria;
+
+import model.graph.Vertex;
+
+/**
+ * Klasa z danymi zamówienia
+ */
+public class Order {
+	private Vertex vertex;
+	
+	/**
+	 * Zwraca wierzcho³ek do którego nale¿y dostarczyæ zamówienie.
+	 * @return wierzcho³ek 
+	 */
+	public Vertex getVertex(){
+		return vertex;
+	}
+}

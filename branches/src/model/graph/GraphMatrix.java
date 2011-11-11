541f753acef21423fdc3c3242e5b3886a5b726d5
orzech.orzechowski <orzech.orzechowski@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-11 21:23:23 +0000

    macierz adiacencji dla algorytmów

--- branches/src/model/graph/GraphMatrix.java
+++ branches/src/model/graph/GraphMatrix.java
@@ -1,5 +1,228 @@
 package model.graph;
 
-public class GraphMatrix {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
+import model.pizzeria.Order;
+
+/**
+ * Klasa zawierajaca macierzow¹(macierz s¹siedztwa) reprezentacje grafu zbudowanego tylko z wierzcho³ków wyró¿nionych.
+ * W wierzcho³kach wyró¿nionych znajduj¹ siê punkty do których nale¿y dostarczyæ pizze i punkt pocz¹tkowy - pizzeria.
+ * Pizzeria zawsze znajduje siê w wierzcho³ku pod indeksem <b>0</b>
+ * @version 1.0
+ */
+public class GraphMatrix {
+	//zawiera wierzcho³ki z oryginalnego grafu
+	private Vertex[] vertexTranslator;
+	//macierz trojkatna
+	private PathData[][] data;
+	private Graph cityMap;
+	
+	/**
+	 * Zwraca now¹ instancje klasy, zawierajac¹ macierzow¹ reprezentacjê wyró¿nionego grafu.
+	 * @param pizzeriaVertex wierzcho³ek w którym znajduje siê pizzeria
+	 * @param oreders lista z zamówieniami, które zostan¹ uwzglêdnione w grafie
+	 * @param cityMap graf reprezentuj¹cy miasto
+	 */
+	public GraphMatrix(Vertex pizzeriaVertex, List<Order> orders, Graph _cityMap){
+		cityMap = _cityMap; 
+		
+		//wyeliminowanie ewentualnych duplikatów
+		Set<Vertex> vertexSet = new HashSet<Vertex>();
+		for(Order order : orders)
+			vertexSet.add(order.getVertex());
+		
+		vertexTranslator = new Vertex[vertexSet.size()+1];
+		vertexTranslator[0] = pizzeriaVertex;
+		
+		int l = 1;
+		for(Vertex vertex : vertexSet)
+			vertexTranslator[l++] = vertex;
+		
+		data = new PathData[vertexTranslator.length][];
+		for(int i = 1; i < vertexTranslator.length; i++)
+			data[i] = new PathData[i];
+
+		for(int i = 1; i < data.length; i++){
+			VertexData[] vertexData = makePath(vertexTranslator[i]);
+			for(int k = 0; k < data[i].length; k++){
+				data[i][k] = getPathData(vertexData, vertexTranslator[i], vertexTranslator[k]);
+			}
+		}
+				
+	}
+
+	//Klasa zawierajaca dane potrzebne do szukania najkrótszych œcie¿ek w grafie
+	private class VertexData implements Comparable<VertexData>{
+		Vertex vertex;
+		Vertex parentVertex;
+		double pathLength;
+		
+		@Override
+		public int compareTo(VertexData arg0) {
+			return (int)(this.pathLength - arg0.pathLength);
+		}
+	}
+	
+	//Klasa zawierajca d³ugoœæ najkrótszej œcie¿ki i kolejne wierzcho³ki na tej œcie¿ce(³¹cznie z pocz¹tkowym i koñcowym
+	private class PathData{
+		double pathLength;
+		//zawiera kolejne wierzcholki w najkrotszej scie¿ce
+		List<Vertex> vertices = new ArrayList<Vertex>();
+	}
+	
+	//zwraca tablice indeksowan¹ numerami z oryginalnego grafu
+	private VertexData[] makePath(Vertex begin) throws IndexOutOfBoundsException{
+		List<Vertex> vertices = cityMap.getVertexList();
+		VertexData[] verticesData = new VertexData[vertices.size()];
+		for(Vertex ver : vertices){
+			verticesData[ver.getNumber()] = new VertexData();
+			verticesData[ver.getNumber()].vertex = ver;
+			verticesData[ver.getNumber()].parentVertex = null;
+			verticesData[ver.getNumber()].pathLength = Double.MAX_VALUE;
+		}
+		
+		verticesData[begin.getNumber()].pathLength = 0.0;
+		Queue<VertexData> vertexQueue = new PriorityQueue<VertexData>(verticesData.length);
+		for(VertexData ver : verticesData)
+			vertexQueue.add(ver);
+		
+		//Dijkstra
+		while(!vertexQueue.isEmpty()){
+			VertexData tmp = vertexQueue.poll();
+			List<Edge> edgeList = tmp.vertex.getEdgeList();
+			for(Edge ed : edgeList){
+				relax(tmp, verticesData[ed.getEnd().getNumber()], ed.getWeight());
+			}
+		}
+		
+		return verticesData;
+	}
+	
+	private void relax(VertexData a, VertexData b, double weight){
+		if(b.pathLength > a.pathLength + weight){
+			b.pathLength = a.pathLength + weight;
+			b.parentVertex = a.vertex;
+		}
+	}
+	
+	private PathData getPathData(VertexData[] data, Vertex begin, Vertex end){
+		PathData result = new PathData();
+		result.pathLength = data[end.getNumber()].pathLength;
+		
+		LinkedList<Vertex> vertices = new LinkedList<Vertex>();
+		vertices.addFirst(end);
+		
+		Vertex currentVertex = data[end.getNumber()].parentVertex;
+		while(currentVertex != null){
+			vertices.addFirst(currentVertex);
+			currentVertex = data[currentVertex.getNumber()].parentVertex;
+		}
+		
+		result.vertices = vertices;
+		return result;
+	}
+	
+	/**
+	 * Zwraca kwadratow¹ macierz adiacencji reprezentuj¹c¹ wyró¿niony graf.
+	 * @return macierz reprezentuj¹ca graf
+	 */
+	public double[][] getMatrix(){
+		double[][] result = new double[data.length][];
+		for(int i = 0; i < result.length; i++)
+			result[i] = new double[data.length];
+		
+		for(int i = 0; i < result.length; i++){
+			for(int k = 0; k < i; k++)
+				result[i][k] = data[i][k].pathLength;
+			
+			result[i][i] = 0.0;
+			
+			for(int k = i+1; k < result[i].length; k++)
+				result[i][k] = data[k][i].pathLength;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Zwraca kopiê tablicy w której znajduj¹ sie wyró¿nione wierzcho³ki. 
+	 * Indeks w tej tablicy, to id wierzcho³ka w macierzy adiacencji zwracanej przez <b>getGrapMatrix()</b>.
+	 * @return tablica wierzcho³ków
+	 */
+	public Vertex[] getVertexTranslator(){
+		return Arrays.copyOf(vertexTranslator, vertexTranslator.length);
+	}
+	
+	/**
+	 * T³umaczy liste wierzcho³ków do odwiedzania na reprezentacjê odpowiednia dla GUI.
+	 * Rozwija drogi pomiedzy wierzcho³kami.
+	 * @param vertices wierzcho³ki do przet³umaczenia
+	 * @return lista wierzcho³ków dla GUI
+	 */
+	public List<Vertex> translateToFullVerticesList(List<Integer> vertices){
+		LinkedList<Vertex> result = new LinkedList<Vertex>();
+		
+		int[] myVertices = new int[vertices.size()];
+		int l = 0;
+		for(Integer myInt : vertices)
+			myVertices[l++] = myInt.intValue();
+		
+		for(int i = 0; i < myVertices.length-1; i++){
+			result.add(vertexTranslator[myVertices[i]]);
+			if(myVertices[i] == myVertices[i+1]){
+				//TODO docelowo gdzie indziej niz System.err
+				System.err.println("Blad algorytmow: ten sam wierzcholek 2x pod rzad, w liscie do odwiedzenia");
+				i++;
+			} else{
+				if(myVertices[i] > myVertices[i+1])
+					result.addAll(data[myVertices[i]][myVertices[i+1]].vertices);
+				else{
+					LinkedList<Vertex> path = (LinkedList<Vertex>)data[myVertices[i+1]][myVertices[i]].vertices;
+					ListIterator<Vertex> iterator = path.listIterator();
+					while(iterator.hasPrevious())
+						result.add(iterator.previous());
+				}
+			}
+		}
+		result.add(vertexTranslator[myVertices[myVertices.length-1]]);
+		
+		return result;
+	}
+	
+	/**
+	 * T³umaczy liste wierzcho³ków do odwiedzania na reprezentacjê odpowiednia dla GUI.
+	 * Rozwija drogi pomiedzy wierzcho³kami.
+	 * @param vertices wierzcho³ki do przet³umaczenia
+	 * @return lista wierzcho³ków dla GUI
+	 */
+	public List<Vertex> translateToFullVerticesList(int[] vertices){
+		LinkedList<Vertex> result = new LinkedList<Vertex>();
+		
+		for(int i = 0; i < vertices.length-1; i++){
+			result.add(vertexTranslator[vertices[i]]);
+			if(vertices[i] == vertices[i+1]){
+				//TODO jesli jest ten sam wierzcho³ek pod rz¹d to co wtedy?
+			} else{
+				if(vertices[i] > vertices[i+1])
+					result.addAll(data[vertices[i]][vertices[i+1]].vertices);
+				else{
+					LinkedList<Vertex> path = (LinkedList<Vertex>)data[vertices[i+1]][vertices[i]].vertices;
+					ListIterator<Vertex> iterator = path.listIterator();
+					while(iterator.hasPrevious())
+						result.add(iterator.previous());
+				}
+			}
+		}
+		result.add(vertexTranslator[vertices[vertices.length-1]]);
+		
+		return result;
+	}
 }

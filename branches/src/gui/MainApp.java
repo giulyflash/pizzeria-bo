05ecfb2fa89903a91279d4ce0c5ba476aaf1efc1
alejajestemmondry@gmail.com <alejajestemmondry@gmail.com@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-15 17:00:12 +0000

    Poprawy w layoucie

--- branches/src/gui/MainApp.java
+++ branches/src/gui/MainApp.java
@@ -7,21 +7,25 @@ import model.graph.Graph;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.ScrollBar;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Spinner;
-import org.eclipse.swt.widgets.Text;
 
 /**
  * Glowne okno aplikacji
@@ -52,7 +56,7 @@ public class MainApp {
 		
 		shell = new Shell(display);//, SWT.SHELL_TRIM & (~SWT.RESIZE));
 		shell.setText(TITLE);
-		canvas = new Canvas(shell, SWT.V_SCROLL | SWT.H_SCROLL);
+		canvas = new Canvas(shell, SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL);
 		
 		shell.setToolTipText("This is tooltip");
 		
@@ -61,6 +65,7 @@ public class MainApp {
 		initUI();
 		
 		shell.open();
+		shell.setSize(800, 600);
 		
 		while(!shell.isDisposed()) {
 			if(!display.readAndDispatch()) {
@@ -74,18 +79,21 @@ public class MainApp {
 	 */
 	public void initUI() {
 		// LAYOUT
-		// TODO layout ladny	
-		shell.setLayout(new FillLayout());
+		// TODO layout ladniejszy	??
+		RowLayout shellLayout = new RowLayout();
+		shellLayout.wrap = false;
+		shellLayout.fill = true;
+		
+		shell.setLayout(shellLayout);
 		
 		// CANVAS
-		canvas.pack();
-		
-		// TODO: zrobic scroolowanie
+		canvas.setLayoutData(new RowData(550, 500));
 		
 		// GROUP
 		// PARAMS
 		Group paramGroup = new Group(shell, SWT.SHADOW_ETCHED_IN);
-		paramGroup.setLayout(new FillLayout(SWT.VERTICAL));
+		paramGroup.setLayout(new RowLayout(SWT.VERTICAL));
+		
 		
 		// PSO
 		Group psoGroup = new Group(paramGroup, SWT.SHADOW_ETCHED_IN);
@@ -101,10 +109,10 @@ public class MainApp {
 		Spinner psoSpinner1 = new Spinner(psoGroup, SWT.WRAP);
 		psoSpinner1.setLocation(100, 20);
 		psoSpinner1.setDigits(2);
-		psoSpinner1.pack();
+		psoSpinner1.setSize(100, 20);
 		
 		// PSO.learning rates
-		Label psoLabel2 = new Label(psoGroup, SWT.LEFT);
+		Label psoLabel2 = new Label(psoGroup, SWT.LEFT);		// TODO: zrobic scroolowanie
 		psoLabel2.setText("c1");
 		psoLabel2.setLocation(5, 45);
 		psoLabel2.pack();
@@ -112,7 +120,8 @@ public class MainApp {
 		Spinner psoSpinner2 = new Spinner(psoGroup, SWT.WRAP);
 		psoSpinner2.setLocation(100, 45);
 		psoSpinner2.setDigits(2);
-		psoSpinner2.pack();
+		psoSpinner2.setSize(100, 20);
+		psoSpinner2.setSize(100, 20);
 		
 		// PSO.learning rates 2
 		Label psoLabel3 = new Label(psoGroup, SWT.LEFT);
@@ -123,7 +132,7 @@ public class MainApp {
 		Spinner psoSpinner3 = new Spinner(psoGroup, SWT.WRAP);
 		psoSpinner3.setLocation(100, 70);
 		psoSpinner3.setDigits(2);
-		psoSpinner3.pack();
+		psoSpinner3.setSize(100, 20);
 		
 		// PSO.particles
 		Label psoLabel4 = new Label(psoGroup, SWT.LEFT);
@@ -134,7 +143,7 @@ public class MainApp {
 		Spinner psoSpinner4 = new Spinner(psoGroup, SWT.WRAP);
 		psoSpinner4.setLocation(100, 95);
 		psoSpinner4.setMaximum(1000);
-		psoSpinner4.pack();
+		psoSpinner4.setSize(100, 20);
 		
 		// PSO.compute
 		Button psoBtn = new Button(psoGroup, SWT.PUSH);
@@ -148,51 +157,35 @@ public class MainApp {
 		gaGroup.setText("Genetyczny");
 		gaGroup.setLocation(100, 100);
 		
-		Label label1 = new Label(gaGroup, SWT.LEFT);
-		label1.setText("Parametr 1");
-		label1.setLocation(5, 20);
-		label1.pack();
-		
-		Text param1 = new Text(gaGroup, SWT.SINGLE);
-		param1.setLocation(80, 20);
-		param1.pack();
-		
-		Button gaOK = new Button(gaGroup, SWT.PUSH);
-		gaOK.setText("Licz!");
-		gaOK.setLocation(90, 50);
-		gaOK.pack();
-		
-		gaGroup.pack();
-		
 		// MENU
 		// TODO zapisywanie wynikow
 		Menu menuBar = new Menu(shell, SWT.BAR);
 		MenuItem cascadeFileMenu = new MenuItem(menuBar, SWT.CASCADE);
-		cascadeFileMenu.setText("&Plik");
+		cascadeFileMenu.setText("&File");
 		
 		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
 		cascadeFileMenu.setMenu(fileMenu);
 		
 		MenuItem loadItem = new MenuItem(fileMenu, SWT.PUSH);
-		loadItem.setText("&OtwĂłrz miasto");
+		loadItem.setText("&Open city");
 		
 		MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
-		exitItem.setText("&WyjĹ›cie");
+		exitItem.setText("&Exit");
 		
 		MenuItem cascadeHelpMenu = new MenuItem(menuBar, SWT.CASCADE);
-		cascadeHelpMenu.setText("&Pomoc");
+		cascadeHelpMenu.setText("&Help");
 		
 		Menu helpMenu = new Menu(shell, SWT.DROP_DOWN);
 		cascadeHelpMenu.setMenu(helpMenu);
 		
 		MenuItem helpItem = new MenuItem(helpMenu, SWT.PUSH);
-		helpItem.setText("&WyĹ›wietl pomoc");
+		helpItem.setText("&Show help");
 		
 		MenuItem aboutItem = new MenuItem(helpMenu, SWT.PUSH);
-		aboutItem.setText("&Autorzy");
+		aboutItem.setText("&Authors");
 		
 		shell.setMenuBar(menuBar);
-		
+		// TODO: zrobic scroolowanie
 		// LISTENERY
 		exitItem.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -207,7 +200,7 @@ public class MainApp {
 			public void widgetSelected(SelectionEvent e) {
 				FileDialog fileDialog = new FileDialog(shell, SWT.OPEN);
 				
-				String [] filterNames = new String[] {"Pliki tekstowe", "Wszystkie pliki (*)"};
+				String [] filterNames = new String[] {"Text files", "All files (*)"};
 				String [] filterExtensions = new String [] { "*.txt", "*"};
 				
 				fileDialog.setFilterNames(filterNames);
@@ -216,11 +209,11 @@ public class MainApp {
 				try {
 					graph = GraphConverter.convert(fileDialog.open());
 					MyPaintListener.addGraph(graph);
-					System.out.println("Wczytano");
+					System.out.println("Loded");
 					canvas.redraw();
 				} catch (ArrayIndexOutOfBoundsException | IOException e1) {
 					MessageBox msgBox = new MessageBox(shell, SWT.ICON_ERROR);
-					msgBox.setMessage("BĹ‚Ä™dny format pliku!");
+					msgBox.setMessage("Bad file format!");
 					msgBox.open();
 				} catch(NullPointerException e2) {
 					
@@ -238,6 +231,8 @@ public class MainApp {
 				messageBox.open();
 			}
 		});
+		
+		// TODO scrollowanie
 	}
 	
 	/**

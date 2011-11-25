5ed0d31912bcc9fa67cc14200e55054e1504f553
pawel.batko@gmail.com <pawel.batko@gmail.com@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-25 22:43:47 +0000

    Improved Crossover (now it's working!)

--- branches/src/algorithms/genetic/operation/crossover/Crossover.java
+++ branches/src/algorithms/genetic/operation/crossover/Crossover.java
@@ -14,180 +14,194 @@ import algorithms.genetic.utils.GeneralUtils;
 
 public class Crossover implements GeneticTransformer {
 
-	private int crossoverProbability;
+	private double crossoverProbability;
 
 	private List<Genome> oldPopulation;
-	
-	public Crossover(int crossoverProbability) {
+
+	private List<Genome> crossoverPopulation;
+
+	private int genomeLength;
+
+	public Crossover(double crossoverProbability, int genomeLength) {
 		this.crossoverProbability = crossoverProbability;
+		this.genomeLength = genomeLength;
 	}
 
 	@Override
 	public Crossover acceptPopulation(List<Genome> population) {
-		this.oldPopulation = population;
+		if (population.size() < 2) {
+			throw new RuntimeException(
+					"GeneticAlgorithm:Crossover: population.size()<2 exception");
+		}
+		this.oldPopulation = new ArrayList<>(population);
 		return this;
 	}
 
 	@Override
 	public List<Genome> generateNewPopulation() {
-		List<Genome> populationPartForCrossover = new ArrayList<>();
+		preparePopulationForCrossover();
+
+		for (int i = 0; i < crossoverPopulation.size(); i += 2) {
+			Genome parent1 = crossoverPopulation.get(i);
+			Genome parent2 = crossoverPopulation.get(i + 1);
+			doEdgeRecombination(parent1, parent2);
+		}
+		return oldPopulation;
+	}
+
+	private void preparePopulationForCrossover() {
+		crossoverPopulation = new ArrayList<>();
 		for (Genome genome : oldPopulation) {
-			if (GeneralUtils.getBoolean(crossoverProbability)) {
-				populationPartForCrossover.add(genome);
+			if (GeneralUtils.generateBoolean(crossoverProbability)) {
+				crossoverPopulation.add(genome);
 			}
 		}
-		oldPopulation.removeAll(populationPartForCrossover);
+		oldPopulation.removeAll(crossoverPopulation);
+		adjustCrossoverPopulationToEvenLength();
+	}
 
-		if (populationPartForCrossover.size() % 2 != 0) {
-			int chosenIndex;
-			Genome tmp;
-			if (GeneralUtils.getBoolean(0.5)) { // remove
-				chosenIndex = GeneralUtils
-						.nextRandomIntModulo(populationPartForCrossover.size());
-				tmp = populationPartForCrossover.remove(chosenIndex);
-				oldPopulation.add(tmp);
-			} else { // add
-				chosenIndex = GeneralUtils.nextRandomIntModulo(oldPopulation
-						.size());
-				tmp = oldPopulation.remove(chosenIndex);
-				populationPartForCrossover.add(tmp);
+	private void adjustCrossoverPopulationToEvenLength() {
+		if (isPopulationForCrossoverOdd()) {
+			if (GeneralUtils.generateBoolean(0.5)) {
+				removeOneGenomeFromCrossoverPopulation();
+			} else {
+				addOneGenomeToCrossoverPopulation();
 			}
 		}
+	}
 
-		List<Genome> newGeneration = new ArrayList<>();
-		for (int i = 0; i < populationPartForCrossover.size(); i++) {
-			Genome parent1 = populationPartForCrossover.get(i);
-			Genome parent2 = populationPartForCrossover.get(i + 1);
-			newGeneration.addAll(doEdgeRecombinationOperator(parent1, parent2));
-			i++;
-		}
-		oldPopulation.addAll(newGeneration);
-		return new ArrayList<>(oldPopulation);
+	private void addOneGenomeToCrossoverPopulation() {
+		int chosenIndex;
+		Genome tmp;
+		chosenIndex = GeneralUtils.nextRandomIntModulo(oldPopulation.size());
+		tmp = oldPopulation.remove(chosenIndex);
+		crossoverPopulation.add(tmp);
+	}
+
+	private void removeOneGenomeFromCrossoverPopulation() {
+		int chosenIndex;
+		Genome tmp;
+		chosenIndex = GeneralUtils.nextRandomIntModulo(crossoverPopulation
+				.size());
+		tmp = crossoverPopulation.remove(chosenIndex);
+		oldPopulation.add(tmp);
+	}
+
+	private boolean isPopulationForCrossoverOdd() {
+		return crossoverPopulation.size() % 2 != 0;
 	}
 
 	public void setCrossoverProbability(int crossoverProbability) {
 		this.crossoverProbability = crossoverProbability;
 	}
 
-	private List<Genome> doEdgeRecombinationOperator(Genome parent1,
-			Genome parent2) {
-		List<Genome> children = new ArrayList<>();
+	private void doEdgeRecombination(Genome parent1, Genome parent2) {
 		int[] path1 = parent1.getPath();
 		int[] path2 = parent2.getPath();
-		int[] childPath1 = generateOneChild(path1, path2);
-		int[] childPath2 = generateOneChild(path2, path1);
+		int[] childPath1 = generateChild(path1, path2);
+		int[] childPath2 = generateChild(path2, path1);
 		Genome child1 = new Genome(childPath1);
 		Genome child2 = new Genome(childPath2);
-		children.add(child1);
-		children.add(child2);
-		return children;
+		oldPopulation.add(child1);
+		oldPopulation.add(child2);
 	}
 
-	private int[] generateOneChild(int[] path1, int[] path2) {
-		int pathLen = path2.length;
-		int[] childPath = new int[pathLen];
-		Map<Integer, Set<Integer>> edgeMap = getCityEdgesMap(path1, path2);
-		List<Integer> notYetChosenCities = getCitiesList(pathLen);
+	private int[] generateChild(int[] path1, int[] path2) {
+		int[] newPath = new int[genomeLength];
+		Map<Integer, Set<Integer>> edgesOfVertexesMap = getEdgesOfVertexMap(
+				path1, path2);
+//		System.out.println("EdgesOfVertexes_Map: " + edgesOfVertexesMap);
+		List<Integer> vertexesLeft = GeneralUtils
+				.getIntegerFromZeroTo(genomeLength);
 
-		int currentCity = path1[0];
+		int currentVertex = path1[0];
 
-		int currentCityIndex = 0;
-		childPath[0] = currentCity;
-		notYetChosenCities.remove(currentCity);
+		int vertexIndex = 0;
 
 		do {
+//			System.out.println("Current vertex: " + currentVertex + " left: " + vertexesLeft);
+			newPath[vertexIndex++] = currentVertex;
+			Set<Integer> neighbours = edgesOfVertexesMap.get(currentVertex);
+			removeCurrentCityFromEdgesMap(edgesOfVertexesMap, currentVertex);
+			vertexesLeft.remove((Object)currentVertex);
+			if(vertexesLeft.size()==0){
+				break;
+			}
+			int nextCity = getNextVertex(edgesOfVertexesMap, neighbours,
+					vertexesLeft);
+			currentVertex = nextCity;
+	//		System.out.println("");
+		} while (!edgesOfVertexesMap.isEmpty());
 
-			Set<Integer> neighbours = edgeMap.get(currentCity);
-			removeCurrentCityFromEdgesMap(edgeMap, currentCity);
-
-			int nextCity = getNextCity(edgeMap, neighbours, notYetChosenCities);
-			currentCityIndex++;
-			currentCity = nextCity;
-			childPath[currentCityIndex] = currentCity;
-			notYetChosenCities.remove(currentCity);
-
-		} while (!edgeMap.isEmpty());
-
-		return childPath;
+		return newPath;
 	}
 
-	private List<Integer> getCitiesList(int pathLen) {
-		List<Integer> notYetChosenCities = new ArrayList<>();
-		for (int i = 0; i < pathLen; i++) {
-			notYetChosenCities.add(i + 1);
-		}
-		return notYetChosenCities;
-	}
-
-	private int getNextCity(Map<Integer, Set<Integer>> edgeMap,
-			Set<Integer> neighbours, List<Integer> notYetChosenCities) {
-		if (neighbours.size() == 0) {
-			Collections.shuffle(notYetChosenCities);
-			return notYetChosenCities.get(0);
-		}
-		int minimalNumberOfNeighbours = 5;
-		int tmpNeighbourEgdeNumber;
-		int candidates[] = new int[4];
-		int i = 0;
-		for (Integer neighbourCity : neighbours) {
-			tmpNeighbourEgdeNumber = edgeMap.get(neighbourCity).size();
-			if (tmpNeighbourEgdeNumber < minimalNumberOfNeighbours) {
-				i = 0;
-				minimalNumberOfNeighbours = tmpNeighbourEgdeNumber;
-			}
-			if (tmpNeighbourEgdeNumber == minimalNumberOfNeighbours) {
-				candidates[i] = neighbourCity;
-				i++;
-			} else {
-				// pass
-			}
-		}
-		int chosenOne;
-		int numberOfCandidates = i;
-		if (i == 1) {
-			chosenOne = candidates[0];
+	private int getNextVertex(Map<Integer, Set<Integer>> edgeMap,
+			Set<Integer> neighbours, List<Integer> vertexesLeft) {
+		int nextVertex;
+//		System.out.println(edgeMap);
+		if (neighbours == null || neighbours.size() == 0) {
+			Collections.shuffle(vertexesLeft);
+			nextVertex = vertexesLeft.get(0);
 		} else {
-			chosenOne = candidates[GeneralUtils
-					.nextRandomIntModulo(numberOfCandidates)];
+			int minimalNumberOfNeighbours = 5;
+			int numberOfNeighbours;
+			int candidates[] = new int[4];
+			int numberOfCandidates = 0;
+			for (Integer vertexWithNeighbours : neighbours) {
+				numberOfNeighbours = edgeMap.get(vertexWithNeighbours).size();
+				if (numberOfNeighbours < minimalNumberOfNeighbours) {
+					numberOfCandidates = 0;
+					minimalNumberOfNeighbours = numberOfNeighbours;
+				}
+				if (numberOfNeighbours == minimalNumberOfNeighbours) {
+					candidates[numberOfCandidates] = vertexWithNeighbours;
+					numberOfCandidates++;
+				} 
+			}
+			if (numberOfCandidates == 1) {
+				nextVertex = candidates[0];
+			} else {
+				nextVertex = candidates[GeneralUtils
+						.nextRandomIntModulo(numberOfCandidates)];
+			}
 		}
-		return chosenOne;
+		return nextVertex;
 	}
 
 	private void removeCurrentCityFromEdgesMap(
-			Map<Integer, Set<Integer>> edgeMap, int initialCity) {
+			Map<Integer, Set<Integer>> edgeMap, int cityToRemove) {
 		Set<Integer> tmp;
 		for (int i : edgeMap.keySet()) {
 			tmp = edgeMap.get(i);
-			tmp.remove(initialCity);
+			tmp.remove(cityToRemove);
 			if (tmp.isEmpty()) {
 				edgeMap.remove(tmp);
 			}
 
 		}
-		edgeMap.remove(initialCity);
+		edgeMap.remove(cityToRemove);
 	}
 
-	private Map<Integer, Set<Integer>> getCityEdgesMap(int[] path1, int[] path2) {
-		Map<Integer, Set<Integer>> egdeList = new LinkedHashMap<>();
-		updateEdgeMapBasedOnPath(path1, egdeList);
-		updateEdgeMapBasedOnPath(path2, egdeList);
-
-		return egdeList;
+	private Map<Integer, Set<Integer>> getEdgesOfVertexMap(int[] path1,
+			int[] path2) {
+		Map<Integer, Set<Integer>> neighbouringVertexesPerVertex = new LinkedHashMap<>();
+		updateEdgeMapBasedOnPath(path1, neighbouringVertexesPerVertex);
+		updateEdgeMapBasedOnPath(path2, neighbouringVertexesPerVertex);
+		return neighbouringVertexesPerVertex;
 	}
 
-	private void updateEdgeMapBasedOnPath(int[] path1,
-			Map<Integer, Set<Integer>> egdeList) {
-		for (int i = 0; i < path1.length; i++) {
-			Set<Integer> edgesOfSpecificCity = egdeList.get(i);
-			if (edgesOfSpecificCity == null) {
-				edgesOfSpecificCity = new HashSet<>();
-				egdeList.put(path1[i], edgesOfSpecificCity);
+	private void updateEdgeMapBasedOnPath(int[] path,
+			Map<Integer, Set<Integer>> edgeList) {
+		for (int i = 0; i < genomeLength; i++) {
+			
+			Set<Integer> edgesOfVertex = edgeList.get(path[i]);
+			if (edgesOfVertex == null) {
+				edgesOfVertex = new HashSet<>();
+				edgeList.put(path[i], edgesOfVertex);
 			}
-			if ((i + 1) != path1.length) {
-				edgesOfSpecificCity.add(path1[i + 1]);
-			} else if (i != 0) {
-				edgesOfSpecificCity.add(path1[i - 1]);
-			}
+			edgesOfVertex.add(path[(i + 1) % genomeLength]);
+			edgesOfVertex.add(path[(genomeLength + i - 1) % genomeLength]);
 		}
 	}
 
---
+++ branches/src/algorithms/genetic/test/CrossoverTest.java
@@ -0,0 +1,61 @@
+package algorithms.genetic.test;
+
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import algorithms.genetic.PopulationGenerator;
+import algorithms.genetic.SolutionEvaluator;
+import algorithms.genetic.operation.crossover.Crossover;
+import algorithms.genetic.structures.GeneticGraph;
+import algorithms.genetic.structures.Genome;
+
+
+public class CrossoverTest{
+
+	GeneticGraph graph;
+	Crossover crossover;
+	List<Genome> population;
+	List<Genome> newPopulation;
+	double crossoverProbability = 0.5;
+	int populationSize = 100;
+	SolutionEvaluator evaluator;
+	double graphMatrix[][] = {{0,4,7,3,872,23,46,1},
+			{2,0,8,324,6,23,75,23},
+			{334,3,0,67,23,2,4,3},
+			{4,3,6,0,7,83,234,7},
+			{5,23,6,23,0,3,25,7},
+			{6,23,7,23,7,0,2,23},
+			{7,12,1,532,123,6,0,6},
+			{7,12,1,532,123,6,1,0}};
+	
+	@Before public void init(){
+		 
+		graph = new GeneticGraph(graphMatrix, graphMatrix.length);
+		int genomeLenght = graphMatrix.length;
+		crossover = new Crossover(crossoverProbability, genomeLenght);
+		int genomeLength = graph.getSize();
+		population = PopulationGenerator.newGenerator(
+				genomeLength, populationSize).generate();
+		evaluator = new SolutionEvaluator(graph);
+		
+	}
+	@Test public void shouldReturnNewPopulation(){
+		
+		
+		
+		newPopulation = crossover.acceptPopulation(population).generateNewPopulation();
+		
+		double populationEval = evaluator.evalPopulationWithReduce(population);
+		System.out.println("old cost" + populationEval);
+		double newPopulationEval = evaluator.evalPopulationWithReduce(newPopulation);
+		System.out.println("new cost" + newPopulationEval);
+		
+		
+		assertTrue(newPopulation.size() == populationSize);
+		
+	}
+}

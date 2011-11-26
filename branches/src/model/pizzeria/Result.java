3361e64532c342512da746235652bee205766986
pawel.batko <pawel.batko@gmail.com>
2011-11-26 11:29:43 +0000

    GeneticAlgorithmRunner added, my custom implementation of Algorithm interface

--- branches/src/model/pizzeria/Result.java
+++ branches/src/model/pizzeria/Result.java
@@ -3,28 +3,35 @@ package model.pizzeria;
 import java.util.ArrayList;
 
 /**
- * Klasa zwracana przez KA¯DY z algorytmów - zawiera listê dostawców Z PRZYDZIELONYMI TRASAMI, oraz dodatkowe dane dla zespo³u GUI do sporz¹dzenia wykresów
+ * Klasa zwracana przez KA¯DY z algorytmów - zawiera listê dostawców Z
+ * PRZYDZIELONYMI TRASAMI, oraz dodatkowe dane dla zespo³u GUI do sporz¹dzenia
+ * wykresów
+ * 
  * @author DoomThrower
- *
+ * 
  */
 public class Result {
 	private ArrayList<DeliveryBoy> _deliveryBoys;
 	private ArrayList<Double> _iterationResults;
-	
+
 	/**
-	 * Ustawia dostawców, którym przypisano œcie¿ki.
-	 * <b>UWAGA</b> Umieszczaæ tylko tych dostawców, którym <b>USTAWIONO</b> œcie¿ki.
+	 * Ustawia dostawców, którym przypisano œcie¿ki. <b>UWAGA</b> Umieszczaæ
+	 * tylko tych dostawców, którym <b>USTAWIONO</b> œcie¿ki.
+	 * 
 	 * @param deliveryBoys
 	 */
 	public void setDeliveryBoys(ArrayList<DeliveryBoy> deliveryBoys) {
 		_deliveryBoys = deliveryBoys;
 	}
+
 	public ArrayList<DeliveryBoy> getDeliveryBoys() {
 		return _deliveryBoys;
 	}
+
 	public void setIterationResults(ArrayList<Double> iterationResults) {
 		_iterationResults = iterationResults;
 	}
+
 	public ArrayList<Double> getIterationResults() {
 		return _iterationResults;
 	}

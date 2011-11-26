422add4af27d5513ccff4c2a4c6c7049e8f52d27
pawel.batko@gmail.com <pawel.batko@gmail.com@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-26 00:24:55 +0000

    Mutation operation implemented

--- branches/src/algorithms/genetic/operation/mutation/Mutation.java
+++ branches/src/algorithms/genetic/operation/mutation/Mutation.java
@@ -1,22 +1,64 @@
 package algorithms.genetic.operation.mutation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.lowagie.text.pdf.ArabicLigaturizer;
 
 import algorithms.genetic.operation.GeneticTransformer;
 import algorithms.genetic.structures.Genome;
+import algorithms.genetic.utils.GeneralUtils;
 
 public class Mutation implements GeneticTransformer{
 
+	private List<Genome> population;
+	
+	private double mutationProbability;
+	
+	private int genomeSize;
+	
+	public Mutation(double mutationProbability, int genomeSize) {
+		this.mutationProbability=mutationProbability;
+		this.genomeSize=genomeSize;
+	}
 	@Override
 	public Mutation acceptPopulation(List<Genome> population) {
-		// TODO Auto-generated method stub
+		this.population=new ArrayList<>(population);
 		return this;
 	}
 
 	@Override
 	public List<Genome> generateNewPopulation() {
-		// TODO Auto-generated method stub
-		return null;
+		for (Genome genome : population) {
+			if(GeneralUtils.generateBoolean(mutationProbability)){
+				int firstCutPoint = GeneralUtils.nextRandomIntModulo(genomeSize);
+				int secondCutPoint = GeneralUtils.nextRandomIntModulo(genomeSize);
+				secondCutPoint=(firstCutPoint+secondCutPoint)%genomeSize;
+				if(firstCutPoint>secondCutPoint){
+					doInversion(genome,secondCutPoint,firstCutPoint);
+				} else if(secondCutPoint>firstCutPoint){
+					doInversion(genome,firstCutPoint,secondCutPoint);
+				}
+			}
+		}
+		return population;
+	}
+	
+	private void doInversion(Genome genome,int first,int second){
+		int j;
+		int i;
+		int[] path = genome.getPath();
+		int tmp;
+		System.out.println("Inverse from: " + first + ", to : " + second);
+		System.out.println(Arrays.toString(path));
+		for (i = first, j= second; j>i; i++,j--) {
+			tmp=path[i];
+			path[i]=path[j];
+			path[j]=tmp;
+		}
+		System.out.println(Arrays.toString(path));
+		
 	}
 
 

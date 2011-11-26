419cf285de5d1e1c46b23ace6563999941a55657
orzech.orzechowski <orzech.orzechowski@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-11-26 19:50:28 +0000

    Zmienilem typy z ArrayList na List i dodalem mozliwosc ustawiania 'timeNeededToFinish' jako double

--- model/pizzeria/Route.java
+++ model/pizzeria/Route.java
@@ -1,21 +1,39 @@
 package model.pizzeria;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.List;
 
 import model.graph.Vertex;
 
 /**
  * Klasa przedstawiaj¹ca trasê, która rozwoziæ pizzê bêdzie dany dostawca.
  * @author DoomThrower
- *
+ * @author orzech
  */
 public class Route {
 	private int _timeNeededToFinish;
-	private ArrayList<Vertex> _vertices;
-	private ArrayList<Order> _orders;
+	private List<Vertex> _vertices;
+	private List<Order> _orders;
 
-	public Route(int timeNeededToFinish, ArrayList<Vertex> vertices, ArrayList<Order> orders) {
+	/**
+	 * @deprecated Bezpieczniej jest uzyc konstruktora w ktorym timeNeededToFinish jest doublem 
+	 * @param timeNeededToFinish
+	 * @param vertices
+	 * @param orders
+	 */
+	public Route(int timeNeededToFinish, List<Vertex> vertices, ArrayList<Order> orders) {
 		_timeNeededToFinish = timeNeededToFinish;
+		_vertices = vertices;
+		_orders = orders;
+	}
+	
+	public Route(double timeNeededToFinish, List<Vertex> vertices, ArrayList<Order> orders){
+		MathContext context = new MathContext(1, RoundingMode.UP);
+		BigDecimal decimal = new BigDecimal(timeNeededToFinish, context);
+		_timeNeededToFinish = decimal.intValue();
 		_vertices = vertices;
 		_orders = orders;
 	}
@@ -23,21 +41,33 @@ public class Route {
 	public int getTimeNeededToFinish() {
 		return _timeNeededToFinish;
 	}
+	
+	/**
+	 * @deprecated Bezpieczniej jest uzyc setera w ktorym timeNeededToFinish jest doublem 
+	 * @param timeNeededToFinish
+	 */
 	public void setTimeNeededToFinish(int timeNeededToFinish) {
 		_timeNeededToFinish = timeNeededToFinish;
 	}
-
-	public ArrayList<Vertex> getVertices() {
+	
+	public void setTimeNeededToFinish(double timeNeedeToFinish){
+		MathContext context = new MathContext(1, RoundingMode.UP);
+		BigDecimal decimal = new BigDecimal(timeNeedeToFinish, context);
+		_timeNeededToFinish = decimal.intValue();
+	}
+	
+	public List<Vertex> getVertices() {
 		return _vertices;
 	}
-	public void setVertices(ArrayList<Vertex> vertices) {
+	public void setVertices(List<Vertex> vertices) {
 		_vertices = vertices;
 	}
 	
-	public ArrayList<Order> getOrders() {
+	public List<Order> getOrders() {
 		return _orders;
 	}
-	public void setOrders(ArrayList<Order> orders) {
+	
+	public void setOrders(List<Order> orders) {
 		_orders = orders;
 	}
 	

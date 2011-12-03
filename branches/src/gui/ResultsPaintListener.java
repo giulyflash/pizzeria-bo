7add00133f288fccd3fdf262a31410126e9d9130
bigm16 <bigm16@gmail.com>
2011-12-03 23:26:57 +0000

    testowe, rysowanie trasy etc. b. wstepna wersja.

--- branches/src/gui/ResultsPaintListener.java
+++ branches/src/gui/ResultsPaintListener.java
@@ -5,30 +5,48 @@ import java.util.ArrayList;
 import model.graph.Edge;
 import model.graph.Graph;
 import model.graph.Vertex;
+import model.pizzeria.Result;
+import model.pizzeria.DeliveryBoy;
 
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 
 /**
- * Klasa do rysowania miasta z zadanego grafu.
- * @author Micha≈Ç Nowak
+ * Klasa do rysowania tras przejazdow
+ * @author Maks Kusak
  *
  */
 public class ResultsPaintListener implements PaintListener {
-	private final static int SIZE = 20;
+	private final static int SIZE = 30;
 	private static Graph graph;
 	private static int SCROLL_X, SCROLL_Y; 
+	private static Result wynik;
+	private static int dostawca;
+	private static int vertex;
 	
 	public ResultsPaintListener() {
 		graph = null;
+		wynik = null;
 		SCROLL_X = 0;
 		SCROLL_Y = 0;
+		dostawca = 0;
+		vertex = 0;
+		
 	}
 	
-	public static void scrollX(int x) {
+	public static void zmienV(int x) {
 		SCROLL_X += x;
 	}
+	
+	public static void zmienD(int x) {
+		dostawca=x;
+	}
+	
+	public static void incV(int x) {
+		vertex=x;
+	}
+	
 	
 	public static void scrollY(int y) {
 		SCROLL_Y += y;
@@ -36,10 +54,17 @@ public class ResultsPaintListener implements PaintListener {
 	
 	/**
 	 * Ustala aktualnie rysowany graf
-	 * @param g - graf
+	 * i trase
 	 */
-	public static void addGraph(Graph g) {
+	public static void rysuj(Graph g, Result w, int i, int j) {
 		graph = g;
+		wynik = w;
+		dostawca= i;
+		vertex = j;
+	}
+	
+	public int min(int x, int y){
+		if(x<y) return x; else return y;
 	}
 	
 	/**
@@ -49,29 +74,78 @@ public class ResultsPaintListener implements PaintListener {
 	public void paintControl(PaintEvent e) {
 	    
 		if(graph != null) {
+			e.gc.setLineWidth(3);
 			e.gc.setBackground(new Color(e.display, 33, 200, 100));
-			ArrayList<Vertex> list = (ArrayList<Vertex>) graph.getVertexList();
-			
+			e.gc.setForeground(new Color(e.display, 0, 0, 0));
+		ArrayList<Vertex> list = (ArrayList<Vertex>) graph.getVertexList();
+		/*				
 			for(Vertex v : list) {
 				for(Edge ed : v.getEdgeList()) {
 					e.gc.drawLine((int)v.getCoordinate().x+(SIZE/2)+SCROLL_X, (int)v.getCoordinate().y+(SIZE/2)+SCROLL_Y, 
 							(int)ed.getEnd().getCoordinate().x+(SIZE/2)+SCROLL_X, 
 							(int)ed.getEnd().getCoordinate().y+(SIZE/2)+SCROLL_Y);
 				}
+			} */
+			// rysuje trasy dla kazdego dostawcy
+			int odchylenie = 2;  //o ile przesunieta bedzie linia wzgledem úroda (zeby sie nie zlewaly)
+			int zmiana = 1;
+			int k=1;
+			System.out.println(dostawca + " "+ vertex);
+			ArrayList<DeliveryBoy> boys = (ArrayList<DeliveryBoy>)wynik.getDeliveryBoys();
+	/*		for(int i=0; i<dostawca-1; i++){
+				for(int j=0;j<4;j++){
+					e.gc.drawLine(i*20,i*50,j*50,j*70);
+				}
 			}
-			// TODO: drugi raz to samo zeby wezly byly na wierzchu. DO POPRAWY ?
 			
+			
+			for(int i=0; i<dostawca; i++){
+				for(int j=0;j<vertex;j++){
+					e.gc.drawLine(i*20,i*50,j*50,j*70);
+				}
+			}*/
+			e.gc.setLineWidth(2);
+			for (int i=0; i<dostawca-1; i++){		
+				e.gc.setForeground(e.display.getSystemColor(k+3));
+			//	System.out.println(odchylenie);
+				
+				ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(i).getCurrentRoute().getVertices();  
+				for (int j=0; j<vlist.size()-1 ; j++){
+					e.gc.drawLine((int)vlist.get(j).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie, 
+							(int)vlist.get(j+1).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j+1).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie);
+				}
+				if(k % 2 == 0) odchylenie += 2; 
+				zmiana = -zmiana;
+				k++;
+			}
+			
+		//	odchylenie = 2;  //o ile przesunieta bedzie linia wzgledem úroda (zeby sie nie zlewaly)
+		//	zmiana = 1;
+		//	k=1;
+			
+			e.gc.setLineWidth(3);	
+			e.gc.setForeground(e.display.getSystemColor(k+3));
+
+				
+			ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(dostawca-1).getCurrentRoute().getVertices();  
+			int min = min(vlist.size(),vertex);
+			for (int j=0; j<min-1 ; j++){
+				e.gc.drawLine((int)vlist.get(j).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie, 
+						(int)vlist.get(j+1).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j+1).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie);
+			}
+			if(k % 2 == 0) odchylenie += 2; 
+			zmiana = -zmiana;
+			k++;
+			
+			e.gc.setForeground(new Color(e.display, 0, 0, 00));
 			for(Vertex v : list) {
 				e.gc.fillOval((int)v.getCoordinate().x+SCROLL_X, (int)v.getCoordinate().y+SCROLL_Y, SIZE, SIZE);
-				
-				// cyferki na wezlach
-				e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4), 
-						(int)v.getCoordinate().y+(SIZE/16), true);
+				e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4)+SCROLL_X, 
+						(int)v.getCoordinate().y+(SIZE/16)+SCROLL_Y, true);
 			}
 			
-			
-			
 		}
+		
 	}
 
 }
---
+++ branches/src/gui/Test.java
@@ -0,0 +1,84 @@
+package gui;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.graph.Vertex;
+import model.pizzeria.DeliveryBoy;
+import model.pizzeria.Order;
+import model.pizzeria.Result;
+import model.pizzeria.Route;
+
+/**
+ * Klasa do testow rysowania trasy
+ * @author Maks Kusak
+ *
+ */
+
+public class Test {
+	private ArrayList<DeliveryBoy> deliveryBoys;
+	private Result wynik;
+	private ArrayList<Double> iterationResults;
+	private Route trasa;
+	private final int CZAS = 10;
+	private List<Vertex> vertices;
+	private List<Order> orders;
+	private DeliveryBoy boy;
+	
+	
+	Result stworz(){
+		wynik = new Result(); 
+		deliveryBoys = new ArrayList<DeliveryBoy>();
+		
+		boy = new DeliveryBoy("boy1",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(1, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy2",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(60,200)));
+		vertices.add(new Vertex(3, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy3",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		
+		boy = new DeliveryBoy("boy6",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		wynik.setDeliveryBoys(deliveryBoys);
+		
+		return wynik;
+		
+	}
+}

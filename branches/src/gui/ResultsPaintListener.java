7ca253d9ec740b2fc99f1d36a129b0eb13a0392b
bigm16@gmail.com <bigm16@gmail.com@34bd533c-0c6c-93a1-5be2-f217dd89695a>
2011-12-04 14:09:46 +0000

    zmiany

--- branches/src/gui/ResultsPaintListener.java
+++ branches/src/gui/ResultsPaintListener.java
@@ -13,9 +13,8 @@ import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 
 /**
- * Klasa do rysowania tras przejazdow
+ * Klasa do rysowania tras przejazdow i grafu
  * @author Maks Kusak
- *
  */
 public class ResultsPaintListener implements PaintListener {
 	private final static int SIZE = 30;
@@ -33,7 +32,6 @@ public class ResultsPaintListener implements PaintListener {
 		SCROLL_Y = 0;
 		dostawca = 0;
 		vertex = 0;
-		
 	}
 	
 	public static void zmienV(int x) {
@@ -48,7 +46,6 @@ public class ResultsPaintListener implements PaintListener {
 		vertex=x;
 	}
 	
-	
 	public static void scrollY(int y) {
 		SCROLL_Y += y;
 	}
@@ -57,10 +54,6 @@ public class ResultsPaintListener implements PaintListener {
 		SCROLL_X += x;
 	}
 	
-	/**
-	 * Ustala aktualnie rysowany graf
-	 * i trase
-	 */
 	public static void rysuj(Graph g, Result w, int i, int j) {
 		graph = g;
 		wynik = w;
@@ -83,6 +76,7 @@ public class ResultsPaintListener implements PaintListener {
 	 */
 	@Override
 	public void paintControl(PaintEvent e) {
+		//rysowanie samego grafu bez sciezki
 		if (dostawca==-1){
 			if(graph != null) {
 				e.gc.setLineWidth(2);
@@ -96,93 +90,87 @@ public class ResultsPaintListener implements PaintListener {
 								(int)ed.getEnd().getCoordinate().y+(SIZE/2)+SCROLL_Y);
 					}
 				}
-				// TODO: drugi raz to samo zeby wezly byly na wierzchu. DO POPRAWY ?
 				
 				for(Vertex v : list) {
 					e.gc.fillOval((int)v.getCoordinate().x+SCROLL_X, (int)v.getCoordinate().y+SCROLL_Y, SIZE, SIZE);
-				//	e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4)+SCROLL_X, 
-					//		(int)v.getCoordinate().y+(SIZE/16)+SCROLL_Y, true);
-				}
-		}
-		}
-			else
-		{
-		if(graph != null) {
-			e.gc.setLineWidth(2);
-			e.gc.setBackground(new Color(e.display, 33, 200, 100));
-			e.gc.setForeground(new Color(e.display, 0, 0, 0));
-		ArrayList<Vertex> list = (ArrayList<Vertex>) graph.getVertexList();
-						
-			for(Vertex v : list) {
-				for(Edge ed : v.getEdgeList()) {
-					e.gc.drawLine((int)v.getCoordinate().x+(SIZE/2)+SCROLL_X, (int)v.getCoordinate().y+(SIZE/2)+SCROLL_Y, 
-							(int)ed.getEnd().getCoordinate().x+(SIZE/2)+SCROLL_X, 
-							(int)ed.getEnd().getCoordinate().y+(SIZE/2)+SCROLL_Y);
-				}
-			} 
-			e.gc.setLineWidth(3);
-			// rysuje trasy dla kazdego dostawcy
-			int odchylenie = 2;  //o ile przesunieta bedzie linia wzgledem œroda (zeby sie nie zlewaly)
-			int zmiana = 1;
-			int k=1;
-			System.out.println(dostawca + " "+ vertex);
-			ArrayList<DeliveryBoy> boys = (ArrayList<DeliveryBoy>)wynik.getDeliveryBoys();
-	/*		for(int i=0; i<dostawca-1; i++){
-				for(int j=0;j<4;j++){
-					e.gc.drawLine(i*20,i*50,j*50,j*70);
+				// rysowanie numerow, niepotrzebne
+				//	e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4)+SCROLL_X,   
+				//		(int)v.getCoordinate().y+(SIZE/16)+SCROLL_Y, true);  
 				}
 			}
-			
-			
-			for(int i=0; i<dostawca; i++){
-				for(int j=0;j<vertex;j++){
-					e.gc.drawLine(i*20,i*50,j*50,j*70);
-				}
-			}*/
-			e.gc.setLineWidth(3);
-			for (int i=0; i<dostawca-1; i++){		
-				e.gc.setForeground(e.display.getSystemColor(k+3));
-			//	System.out.println(odchylenie);
+		}else{
+			// rysowanie grafu i sciezki 
+			if(graph != null) {
+				e.gc.setLineWidth(2);
+				e.gc.setBackground(new Color(e.display, 33, 200, 100));
+				e.gc.setForeground(new Color(e.display, 0, 0, 0));
+				ArrayList<Vertex> list = (ArrayList<Vertex>) graph.getVertexList();
+				// rysuje graf podstawowy
+				for(Vertex v : list) {
+					for(Edge ed : v.getEdgeList()) {
+						e.gc.drawLine((int)v.getCoordinate().x+(SIZE/2)+SCROLL_X, (int)v.getCoordinate().y+(SIZE/2)+SCROLL_Y, 
+								(int)ed.getEnd().getCoordinate().x+(SIZE/2)+SCROLL_X, 
+								(int)ed.getEnd().getCoordinate().y+(SIZE/2)+SCROLL_Y);
+					}
+				} 
 				
-				ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(i).getCurrentRoute().getVertices();  
-				for (int j=0; j<vlist.size()-1 ; j++){
+				// procedura testowa, zostawiam na pozniej
+				/*for(int i=0; i<dostawca-1; i++){
+					for(int j=0;j<4;j++){
+						e.gc.drawLine(i*20,i*50,j*50,j*70);
+					}
+				}
+				for(int i=0; i<dostawca; i++){
+					for(int j=0;j<vertex;j++){
+						e.gc.drawLine(i*20,i*50,j*50,j*70);
+					}
+				}*/
+		
+				
+				// rysuje trasy dla kazdego dostawcy
+				e.gc.setLineWidth(3);
+				int odchylenie = 2; 
+				int zmiana = 1;
+				int k=1;
+				System.out.println(dostawca + " "+ vertex);
+				ArrayList<DeliveryBoy> boys = (ArrayList<DeliveryBoy>)wynik.getDeliveryBoys();
+				
+				for (int i=0; i<dostawca-1; i++){		
+					e.gc.setForeground(e.display.getSystemColor(k+3));
+					ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(i).getCurrentRoute().getVertices();  
+					for (int j=0; j<vlist.size()-1 ; j++){
+						e.gc.drawLine((int)vlist.get(j).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie, 
+								(int)vlist.get(j+1).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j+1).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie);
+					}
+					if(k % 2 == 0) odchylenie += 2; 
+					zmiana = -zmiana;
+					k++;
+				}
+				
+				e.gc.setForeground(e.display.getSystemColor(k+3));
+				ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(dostawca-1).getCurrentRoute().getVertices();  
+				int min = min(vlist.size(),vertex);
+				for (int j=0; j<min-1 ; j++){
 					e.gc.drawLine((int)vlist.get(j).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie, 
 							(int)vlist.get(j+1).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j+1).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie);
+					
 				}
-				if(k % 2 == 0) odchylenie += 2; 
-				zmiana = -zmiana;
-				k++;
-			}
-			
-		//	odchylenie = 2;  //o ile przesunieta bedzie linia wzgledem œroda (zeby sie nie zlewaly)
-		//	zmiana = 1;
-		//	k=1;
-			
-			e.gc.setLineWidth(3);	
-			e.gc.setForeground(e.display.getSystemColor(k+3));
-			
-
-			ArrayList<Vertex> vlist = (ArrayList<Vertex>)boys.get(dostawca-1).getCurrentRoute().getVertices();  
-			int min = min(vlist.size(),vertex);
-			for (int j=0; j<min-1 ; j++){
-				e.gc.drawLine((int)vlist.get(j).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie, 
-						(int)vlist.get(j+1).getCoordinate().x+(SIZE/2)+SCROLL_X+zmiana*odchylenie, (int)vlist.get(j+1).getCoordinate().y+(SIZE/2)+SCROLL_Y+zmiana*odchylenie);
 				
-			}
-
-			
-			e.gc.setForeground(new Color(e.display, 0, 0, 0));
-			e.gc.setBackground(new Color(e.display, 33, 200, 100));
-			for(Vertex v : list) {
-				e.gc.fillOval((int)v.getCoordinate().x+SCROLL_X, (int)v.getCoordinate().y+SCROLL_Y, SIZE, SIZE);
-			//	e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4)+SCROLL_X, 
-				//		(int)v.getCoordinate().y+(SIZE/16)+SCROLL_Y, true);
-			}
-			e.gc.setBackground(new Color(e.display, 250, 250, 250));
-			e.gc.fillOval((int)vlist.get(min-1).getCoordinate().x+SCROLL_X+(SIZE - SIZEK)/2, (int)vlist.get(min-1).getCoordinate().y+SCROLL_Y+(SIZE - SIZEK)/2, SIZEK, SIZEK);
-			e.gc.drawText(""+(dostawca), (int)vlist.get(min-1).getCoordinate().x+SCROLL_X+12, (int)vlist.get(min-1).getCoordinate().y+SCROLL_Y+8, true);
-		}	
-	}
+				e.gc.setForeground(new Color(e.display, 0, 0, 0));
+				e.gc.setBackground(new Color(e.display, 33, 200, 100));
+				for(Vertex v : list) {
+					e.gc.fillOval((int)v.getCoordinate().x+SCROLL_X, (int)v.getCoordinate().y+SCROLL_Y, SIZE, SIZE);
+					// rysowanie numerow przy wezlach, zbedne
+					//	e.gc.drawText("" + v.getNumber(), (int)v.getCoordinate().x+(SIZE/4)+SCROLL_X,   
+					//		(int)v.getCoordinate().y+(SIZE/16)+SCROLL_Y, true);
+				}
+				
+				// rysowanie znacznika miejsca w ktorym znajduje sie dostawca, oraz jego numeru
+				e.gc.setBackground(new Color(e.display, 250, 250, 250));
+				e.gc.fillOval((int)vlist.get(min-1).getCoordinate().x+SCROLL_X+(SIZE - SIZEK)/2, (int)vlist.get(min-1).getCoordinate().y+SCROLL_Y+(SIZE - SIZEK)/2, SIZEK, SIZEK);
+				e.gc.drawText(""+(dostawca), (int)vlist.get(min-1).getCoordinate().x+SCROLL_X+12, (int)vlist.get(min-1).getCoordinate().y+SCROLL_Y+8, true);
+			}	
+		}
 	}
 
 }

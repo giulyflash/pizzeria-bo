9e019a0222da0afce4070cb609c8b7e2330cdd90
bigm16 <bigm16@gmail.com>
2011-12-10 18:04:02 +0000

    praca wre

--- gui/Test.java
+++ gui/Test.java
@@ -88,6 +88,123 @@ public class Test {
 		boy.setCurrentRoute(trasa);
 		deliveryBoys.add(boy);
 		
+	/*	boy = new DeliveryBoy("boy6",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(1, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		
+		boy = new DeliveryBoy("boy7",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy8",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(60,200)));
+		vertices.add(new Vertex(3, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy9",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy10",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(200,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy1",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(1, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy2",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(60,200)));
+		vertices.add(new Vertex(3, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy3",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy4",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		
+		boy = new DeliveryBoy("boy5",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
 		boy = new DeliveryBoy("boy6",5);
 		vertices = new ArrayList<Vertex>();
 		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
@@ -121,6 +238,148 @@ public class Test {
 		trasa = new Route(CZAS, vertices,null);
 		boy.setCurrentRoute(trasa);
 		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy9",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy10",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(200,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy1",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(1, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy2",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(60,200)));
+		vertices.add(new Vertex(3, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy3",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy4",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		
+		boy = new DeliveryBoy("boy5",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy6",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(1, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		
+		boy = new DeliveryBoy("boy7",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy8",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(60,200)));
+		vertices.add(new Vertex(3, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices,null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy9",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(4, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);
+		
+		boy = new DeliveryBoy("boy10",5);
+		vertices = new ArrayList<Vertex>();
+		vertices.add(new Vertex(0, new Point2D.Double(10,20)));
+		vertices.add(new Vertex(1, new Point2D.Double(60,140)));
+		vertices.add(new Vertex(2, new Point2D.Double(120,70)));
+		vertices.add(new Vertex(3, new Point2D.Double(200,140)));
+		vertices.add(new Vertex(4, new Point2D.Double(250,60)));
+		vertices.add(new Vertex(1, new Point2D.Double(80,40)));
+		vertices.add(new Vertex(4, new Point2D.Double(10,20)));
+		trasa = new Route(CZAS, vertices, null);
+		boy.setCurrentRoute(trasa);
+		deliveryBoys.add(boy);*/
 		
 		wynik.setDeliveryBoys(deliveryBoys);
 		
